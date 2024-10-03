ca49f039273fbcd46be59af2b02586baecd1104e
isabel <isabel@isabelroses.com>
2024-10-03 14:03:23 +0100

    fixup! fix stuff ig

--- modules/base/users/comfy.nix
+++ modules/base/users/comfy.nix
@@ -11,11 +11,9 @@ in
 {
   users.users.comfy =
     {
-      openssh.authorizedKeys.keys = [
-        "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIMQDiHbMSinj8twL9cTgPOfI6OMexrTZyHX27T8gnMj2"
-      ];
+      openssh.authorizedKeys.keys = [ ];
 
-      home = "/${ldTernary pkgs "home" "Users"}/isabel";
+      home = "/${ldTernary pkgs "home" "Users"}/comfy";
       shell = pkgs.fish;
     }
     // (ldTernary pkgs {
--- modules/base/users/root.nix
+++ modules/base/users/root.nix
@@ -3,8 +3,6 @@
   users.users.root = lib.modules.mkIf pkgs.stdenv.hostPlatform.isLinux {
     initialPassword = "changeme";
 
-    openssh.authorizedKeys.keys = [
-      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIMQDiHbMSinj8twL9cTgPOfI6OMexrTZyHX27T8gnMj2"
-    ];
+    openssh.authorizedKeys.keys = [ ];
   };
 }
