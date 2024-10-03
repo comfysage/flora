292c46285d459289cc7814a90805fc269677f081
isabel <isabel@isabelroses.com>
2024-10-03 14:03:23 +0100

    fixup! fix stuff ig

--- home/comfy/system/docs.nix
+++ home/comfy/system/docs.nix
@@ -4,9 +4,7 @@ let
   inherit (lib.modules) mkForce;
 in
 {
-  # This is enabled by default, but we don't want it
-  # NOTE: this will break some programs that call `man` for you like `nix-collect-garbage`
-  programs.man.enable = false;
+  programs.man.enable = true;
 
   # I don't use docs, so just disable them
   manual = mapAttrs (_: mkForce) {
